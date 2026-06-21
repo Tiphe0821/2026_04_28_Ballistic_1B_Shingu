11db3719e737b9e1518b29af1c95be4492e54ca0
cocomakid <joonyoung0113@gmail.com>
2026-06-21 20:37:13 +0900

    [Fix] Singleton

--- Ballistic_Team/Assets/Scripts/Sound/AudioManager.cs
+++ Ballistic_Team/Assets/Scripts/Sound/AudioManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
     [Header("---------- Audio Source ----------")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
@@ -11,10 +14,38 @@ public class AudioManager : MonoBehaviour
     public AudioClip backGround;
     public AudioClip Click;
 
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
     private void Start()
     {
         musicSource.clip = backGround;
         musicSource.Play();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        AddClickSoundToAllButtons();
+    }
+
+    private void AddClickSoundToAllButtons()
+    {
+        Button[] allButtons = FindObjectsOfType<Button>(true); // 비활성 오브젝트 포함하려면 true
+        foreach (Button btn in allButtons)
+        {
+            btn.onClick.AddListener(PlayClickSound);
+        }
     }
 
     public void PlaySFX(AudioClip clip)
